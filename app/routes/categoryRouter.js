26585f5697fc792e7528eee6a8a21f4929612e2b
themrzlyv <themirzaliyev@gmail.com>
2021-02-25 22:22:12 +0400

    server is ready for now

--- app/routes/categoryRouter.js
+++ app/routes/categoryRouter.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { getAllCategories, getSingleCategory , createCategory, updateCategory, deleteCategory, getPostsFromSingleCategory} from '../controllers/categoryController.js'
+import { getAllCategories, getSingleCategory , createCategory, updateCategory, deleteCategory} from '../controllers/categoryController.js'
 import { Auth, getAdminAccess } from '../helpers/middlewares/Authorization.js'
 
 const categoryRouter = express.Router()
 
 categoryRouter.get("/", getAllCategories)
-categoryRouter.get("/:id", getSingleCategory)
+categoryRouter.get("/:id",Auth,getAdminAccess, getSingleCategory)
 categoryRouter.post("/",Auth,getAdminAccess, createCategory)
 categoryRouter.put("/:id",Auth,getAdminAccess, updateCategory)
 categoryRouter.delete("/:id",Auth,getAdminAccess, deleteCategory)
