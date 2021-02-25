eed7d3bbd0b4ffbfcd9ec7336181c0c5cf565323
themrzlyv <themirzaliyev@gmail.com>
2021-02-25 22:18:09 +0400

    post routes and errorhandler is updated

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -1,6 +1,5 @@
 import Post from '../models/Post.js'
 import { postValidator } from '../helpers/ErrorHandler.js'
-import Category from '../models/Category.js'
 
 export const getAllPosts = async (req,res) => {
     try {
@@ -28,21 +27,14 @@ export const getSinglePost = async (req,res) => {
 export const createNewPost = async(req,res) => {
     try {
         //get datas from request
-        const {title,category,description} = req.body
+        const {title,category,description,image} = req.body
 
         // checking post values
-        const error = postValidator(title,category,description)
+        const error = postValidator(title,category,description,image)
         if(error) return res.status(400).json({error: error})
 
         // creating new post
-        const post = await Post({title,category,description}).save()
-
-        // find category with post's category id
-        const categorylist = await Category.findById({_id: category})
-
-        // push new post to category and save
-        categorylist.posts.push(post)
-        await categorylist.save()
+        const post = await Post({title,category,description,image}).save()
 
         return res.status(200).json({success: true})
     } catch (error) {
@@ -53,13 +45,13 @@ export const createNewPost = async(req,res) => {
 export const updatePost = async (req,res) => {
     try {
         const {id} = req.params
-        const {title,category, description} = req.body
+        const {title,category, description,image} = req.body
         
         //checking any error
-        const error = postValidator(title,category,description);
+        const error = postValidator(title,category,description,image);
         if(error) return res.status(400).json({error: error});
 
-        const post = await Post.findByIdAndUpdate({_id: id}, {title,category,description},
+        const post = await Post.findByIdAndUpdate({_id: id}, {title,category,description,image},
             {
                 new: true,
                 runValidators: true
@@ -76,15 +68,6 @@ export const deletePost = async (req,res) => {
         // get post id from request params
         const {id} = req.params
         const post = await Post.findByIdAndDelete({_id: id})
-
-        // find post's category 
-        let category = await Category.findById({_id: post.category})
-
-        //remove selected post from category's posts
-        category.posts = category.posts.filter(item => item != id)
-        // save category
-        await category.save()
-
         res.status(200).json({success: true});
     } catch (error) {
         return res.status(500).json({error: error.message})
