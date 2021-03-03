e113511ee369b84e42b52ef8ac8f91d90aca1a6a
themrzlyv <themirzaliyev@gmail.com>
2021-03-03 22:21:03 +0400

    updated server js

--- app/server.js
+++ app/server.js
@@ -18,6 +18,13 @@ app.use(cookieParser());
 
 app.use('/', routes);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
